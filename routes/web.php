0519d92545b4766aa69cadae5e6e3178b362496d
aldiui <aldijaya280902@gmail.com>
2024-04-20 17:59:23 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
 });
